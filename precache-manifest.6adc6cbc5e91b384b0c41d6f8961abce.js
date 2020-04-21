a425076d9913eeb29de2eb316610fffb448e090f
Diatchik <anastasia.diatchik91@mail.ru>
2020-04-21 10:21:14 +0300

    Updates

---
+++ precache-manifest.6adc6cbc5e91b384b0c41d6f8961abce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af8ed013565f9a59215fc95d200a477",
+    "url": "/dashboard-genset/index.html"
+  },
+  {
+    "revision": "183b27e6efcc4b1ea674",
+    "url": "/dashboard-genset/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9e5b0e8738053359308c",
+    "url": "/dashboard-genset/static/css/main.c99e30ba.chunk.css"
+  },
+  {
+    "revision": "183b27e6efcc4b1ea674",
+    "url": "/dashboard-genset/static/js/2.35b94721.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dashboard-genset/static/js/2.35b94721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5b0e8738053359308c",
+    "url": "/dashboard-genset/static/js/main.a48fa7d8.chunk.js"
+  },
+  {
+    "revision": "fd4471af51e17c9f17ea",
+    "url": "/dashboard-genset/static/js/runtime-main.69545385.js"
+  },
+  {
+    "revision": "c1ed66a35bae9dad14de791afe4f14df",
+    "url": "/dashboard-genset/static/media/bar-chart.c1ed66a3.svg"
+  },
+  {
+    "revision": "908fa69cada7beb6848c753f71639a9e",
+    "url": "/dashboard-genset/static/media/card-checklist.908fa69c.svg"
+  },
+  {
+    "revision": "3c226ca828f9800bcf29bc8c53df6075",
+    "url": "/dashboard-genset/static/media/fuel-level.3c226ca8.svg"
+  },
+  {
+    "revision": "fdbd49938280d8f92b60810e4da68b27",
+    "url": "/dashboard-genset/static/media/gear.fdbd4993.svg"
+  },
+  {
+    "revision": "59b84587c87b84740d1d26a6474881a6",
+    "url": "/dashboard-genset/static/media/oil-level.59b84587.svg"
+  },
+  {
+    "revision": "4e61cdc162b9be98a8e75934af944138",
+    "url": "/dashboard-genset/static/media/temperature.4e61cdc1.svg"
+  }
+]);
